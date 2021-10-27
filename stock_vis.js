ab24a34577c5199af882960944a7d3e70f6a22b7
jonpage3 <jonpage3@gmail.com>
2021-10-27 19:44:40 -0400

    trying to figure out js setup

--- stock_vis.js
+++ stock_vis.js
@@ -2,7 +2,13 @@
 class StockVis {
 
     constructor(svg_id,data){
-
+        this.svg_id = svg_id;
+        this.data = data;
     }
 
+    render() {
+
+        let svg = d3.select("#"+this.svg_id);
+
+    }
 }
